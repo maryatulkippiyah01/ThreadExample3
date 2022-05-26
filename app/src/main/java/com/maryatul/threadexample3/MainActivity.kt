86d01f3863cc60c54b135d7b3140ef193226a675
JALA MEDIA COMP <maryatulkippiyah01@gmail.com>
2022-05-26 20:12:41 +0700

    percobaan ThreadExample3

---
+++ app/src/main/java/com/maryatul/threadexample3/MainActivity.kt
@@ -0,0 +1,16 @@
+
+package com.maryatul.threadexample3
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    fun startThread(view: View):Unit{
+        val runnable = ExampleRunnable(10);
+        Thread(runnable).start()
+    }
+}
